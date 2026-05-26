443eb5b35b15cbd06938b2adfc8f353318472479
Jonathan Gao <gsmlg.com@gmail.com>
2026-05-26 12:49:49 +0800

    test(memory): start oban for memory tests

--- apps/backplane_memory/test/test_helper.exs
+++ apps/backplane_memory/test/test_helper.exs
@@ -1,3 +1,8 @@
 {:ok, _} = Application.ensure_all_started(:backplane_system)
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(Application.fetch_env!(:backplane_memory, :repo), :manual)
+
+{:ok, _} =
+  :backplane
+  |> Application.fetch_env!(Oban)
+  |> Oban.start_link()
