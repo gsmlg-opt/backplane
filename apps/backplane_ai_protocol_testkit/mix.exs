19d6bac303a16f6ecfd06ff223c0c9bd66adec2b
Jonathan Gao <gsmlg.com@gmail.com>
2026-09-17 13:20:44 +0800

    chore(release): bump version to 0.10.0

--- apps/backplane_ai_protocol_testkit/mix.exs
+++ apps/backplane_ai_protocol_testkit/mix.exs
@@ -1,7 +1,7 @@
 defmodule Backplane.AiProtocolTestkit.MixProject do
   use Mix.Project
 
-  @version "0.9.0"
+  @version "0.10.0"
   @source_url "https://github.com/gsmlg-opt/backplane/tree/main/apps/backplane_ai_protocol_testkit"
 
   def project do
--- apps/backplane_skills/mix.exs
+++ apps/backplane_skills/mix.exs
@@ -4,7 +4,7 @@ defmodule BackplaneSkills.MixProject do
   def project do
     [
       app: :backplane_skills,
-      version: "0.9.0",
+      version: "0.10.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
